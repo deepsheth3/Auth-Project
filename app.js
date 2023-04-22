add3730aa61c5d52a6508a4ce5af4e84783115fd
deep sheth <deepsheth3@gmail.com>
2023-04-22 12:53:20 +0530

    Auth page with Level 1 security

---
+++ app.js
@@ -0,0 +1,65 @@
+//jshint esversion:6
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB').then(()=>{
+    console.log('Connection Successful')
+})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String})
+
+const secret = 'Thisisastringnoonecanguessitsmysecret'
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']})
+
+const User = new mongoose.model('user',userSchema)
+
+app.get('/',function(req,res){
+    res.render('home')
+});
+
+app.get('/login',function(req,res){
+    res.render('login')
+});
+
+app.get('/register',function(req,res){
+    res.render('register')
+});
+
+app.post('/register', function(req,res){
+    
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save().then(()=>{
+        res.render('secrets')
+    })
+})
+
+app.post('/login', function(req,res){
+    
+    User.findOne({
+        email: req.body.username
+    }).then((foundUser)=>{
+        if (foundUser.password === req.body.password){
+            res.render('secrets')
+        }else{
+            res.redirect('login')
+        }
+    })
+})
+
+app.listen(3000,function(){
+    console.log('Listing on port 3000')
+})
