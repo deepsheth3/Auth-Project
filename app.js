ade5d8ec4b0319655d1dc5cbe69eeac9191601c1
deep sheth <deepsheth3@gmail.com>
2023-04-22 20:27:20 +0530

    Added hashing for security

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 
 const app = express()
 
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String})
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
 
 const User = new mongoose.model('user',userSchema)
 
@@ -40,7 +39,7 @@ app.post('/register', function(req,res){
     
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save().then(()=>{
         res.render('secrets')
@@ -52,7 +51,7 @@ app.post('/login', function(req,res){
     User.findOne({
         email: req.body.username
     }).then((foundUser)=>{
-        if (foundUser.password === req.body.password){
+        if (foundUser.password === md5(req.body.password)){
             res.render('secrets')
         }else{
             res.redirect('login')
